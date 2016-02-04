ddaf28ab06c56dd5c67ffa9c90c06864f0055f93
Bassim <bassim_bashir@hotmail.com>
2016-02-04 11:55:25 +0100

    Creating the archetype and the prototype poms

---
+++ src/main/resources/archetype-resources/src/main/java/DataTransformerSample.java
@@ -0,0 +1,12 @@
+package de.rwthaachen.openlap;
+
+import DataSet.OLAPDataSet;
+import de.rwthaachen.openlap.visualizer.framework.model.TransformedData;
+import de.rwthaachen.openlap.visualizer.framework.DataTransformer;
+
+public class DataTransformerSample implements DataTransformer {
+
+    public TransformedData transformData(OLAPDataSet olapDataSet) {
+        return new TransformedData();
+    }
+}
---
+++ src/main/resources/archetype-resources/src/main/java/VisualizationCodeGeneratorSample.java
@@ -0,0 +1,19 @@
+package de.rwthaachen.openlap;
+
+import DataSet.OLAPDataSet;
+import de.rwthaachen.openlap.visualizer.framework.model.TransformedData;
+import de.rwthaachen.openlap.visualizer.framework.VisualizationCodeGenerator;
+
+public class VisualizationCodeGeneratorSample extends VisualizationCodeGenerator {
+
+    @Override
+    protected void initializeDataSetConfiguration() {
+        this.setInput(new OLAPDataSet());
+        this.setOutput(new OLAPDataSet());        
+    }
+
+    @Override
+    protected String visualizationCode(TransformedData transformedData) {
+        return "";
+    }
+}
