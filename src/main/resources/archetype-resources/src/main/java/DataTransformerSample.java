6b440a75d6bc37f7ad772c2f2883598a7fd9e1b3
Bassim <bassim_bashir@hotmail.com>
2016-03-10 20:41:00 +0100

    Added comments to the Sample files

--- src/main/resources/archetype-resources/src/main/java/DataTransformerSample.java
+++ src/main/resources/archetype-resources/src/main/java/DataTransformerSample.java
@@ -4,6 +4,10 @@ import DataSet.OLAPDataSet;
 import de.rwthaachen.openlap.visualizer.framework.model.TransformedData;
 import de.rwthaachen.openlap.visualizer.framework.DataTransformer;
 
+/**
+* This class servces as an example of a DataTransformer. The transformData function should process the provided
+* OLAPDataSet and return a TransformedData<?> object which can be used by the visualization code
+*/
 public class DataTransformerSample implements DataTransformer {
 
     public TransformedData<?> transformData(OLAPDataSet olapDataSet) {
--- src/main/resources/archetype-resources/src/main/java/VisualizationCodeGeneratorSample.java
+++ src/main/resources/archetype-resources/src/main/java/VisualizationCodeGeneratorSample.java
@@ -4,6 +4,18 @@ import DataSet.OLAPDataSet;
 import de.rwthaachen.openlap.visualizer.framework.model.TransformedData;
 import de.rwthaachen.openlap.visualizer.framework.VisualizationCodeGenerator;
 
+/**
+* A sample class which serves as an example of a VisualizationCodeGenerator. 
+*
+* The initializeDataSetConfiguration method should contain the input OLAPDataSet configuration (the actual data is not required). 
+* The client (AnalyticsEngine) will use this input configuration to verify if the VisualizationCodeGenerator can accept the 
+* OLAPDataSet it wants to send. If and only if the input configuration matches that what the client (AnalyticsEngine) wants to 
+* send the actual data will be sent. The output OLAPDataSet configuration for a Visualization Method (VisualizationCodeGenerator) is 
+* not important.
+*
+* The visualizationCode method should return the actual visualization code as a String to be sent back to the client by making
+* use of the provided TransformedData<?>.
+*/
 public class VisualizationCodeGeneratorSample extends VisualizationCodeGenerator {
 
     @Override
